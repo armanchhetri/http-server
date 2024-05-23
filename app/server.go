48dad01c7b1ff77d70987d1611b3fae73060ade4
Arman Chhetri <armanchhetri44@gmail.com>
2024-05-23 17:07:03 +0545

    fix: conten-type header

--- app/server.go
+++ app/server.go
@@ -22,6 +22,7 @@ type MyApp struct{}
 
 func (app MyApp) UserAgentHandler(rw http.ResponseWriter, r *http.Request) {
 	userAgent := r.Header["User-Agent"]
+	rw.WriteHeader("Content-Type", "text/plain")
 	// fmt.Println("got: ", userAgent)
 	rw.Write([]byte(userAgent))
 }
